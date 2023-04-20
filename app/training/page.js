ee17c90b9b728f2bc7a02100cbd0a1d8369e8cf6
Saud Khan <khansaud3551@gmail.com>
2023-04-21 04:27:16 +0500

    #padding corrected

--- app/training/page.js
+++ app/training/page.js
@@ -180,7 +180,7 @@ const ClientReview = ({ img }) => {
   return (
     <>
       <div className="col-span-12 md:col-span-6 lg:col-span-4">
-        <div className="p-8 space-y-3">
+        <div className="px-8 py-2 md:p-8 space-y-3">
           <div className="flex gap-2 mb-2">
             <img src="assets/icon-star.svg" alt="" />
             <img src="assets/icon-star.svg" alt="" />
